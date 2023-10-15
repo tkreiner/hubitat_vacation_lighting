6ab44c3ae8a878fea7c27e652ad9d327b843beba
Tom Kreiner <31622763+tkreiner@users.noreply.github.com>
2023-10-14 23:28:03 -0400

    Select Modes and Switches / Begin Switch Config Pages

--- app/hubitat_vacation_lighting.groovy
+++ app/hubitat_vacation_lighting.groovy
@@ -11,12 +11,35 @@ definition(
 )
 
 preferences {
-    page(name: "mainPage", title: "Vacation Lighting", install: true, uninstall: true) {
+    page(name: "mainPage", title: "Vacation Lighting", install: true, uninstall: true)
+    page(name: "configSwitch", title: "Configure Switch", uninstall: true)
+}
+
+// Define the main page of the app
+def mainPage() {
+	dynamicPage(name: "mainPage", title: "Mode Switches", uninstall: true, install: true) {
         section {
             paragraph "Welcome to the Vacation Lighting app."
-        }
-        section("Select a Mode") {
             paragraph "Select the mode that will be used to activate this lighting."
+            input "modes", "mode", title: "Select Modes", multiple: true, submitOnChange: true, required: true
+            paragraph "Select the switches that you want to control."
+            input "switches", "capability.switch", title: "Select Switches", multiple: true, submitOnChange: true, required: true
+            if(switches) {
+                switches.each{dev ->
+                    paragraph dev.getLabel()
+                    href name: "configSwitch", page: "configSwitch", title: "Setup Schedule", params: [deviceId: dev.id]
+                }
+            }
+        }
+	}
+}
+
+// Define page that sets up lighting for a switch
+def configSwitch(params) {
+    dynamicPage(name: "configSwitch", title: "Configure Switch", uninstall: true) {
+        section {
+            paragraph "Configure lighting schedule for ${params.deviceId}"
+            href name: "returnMain", page: "mainPage", title: "Go to Setup Page"
         }
     }
 }
@@ -25,15 +48,27 @@ preferences {
 def installed() {
   // for now, just write entry to "Logs" when it happens:
     log.trace "installed()"
+    updated()
 }
 
 // Called when user presses "Done" button in app
 def updated() {
     log.trace "updated()"
+    subscribe(location, modeHandler)
 }
 
 // Called when app uninstalled
 def uninstalled() {
    log.trace "uninstalled()"
    // Most apps would not need to do anything here
+}
+
+// Detect when mode has changed
+def modeHandler(evt) {
+    log.debug "modeHandler() called: ${evt.name} ${evt.value}"
+    if (modes.contains(evt.value)) {
+        log.debug "Enabling vacation lighting."
+    } else {
+        log.debug "Disabling vacation lighting."
+    }
 }
